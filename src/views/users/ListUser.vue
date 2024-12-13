505d9a7e583c81476cd1ef7be3b4842db97da487
thillo <salysem90@gmail.com>
2024-12-13 08:45:26 +0100

    CRUD for Users added

--- src/views/users/ListUser.vue
+++ src/views/users/ListUser.vue
@@ -1,54 +1,54 @@
 <script setup>
 
-import { useRouter } from 'vue-router';
-const router = useRouter()
-import { useGestionStore } from '../../store/gestion';
-import { ref } from 'vue';
+// import { useRouter } from 'vue-router';
+// const router = useRouter()
+// import { useGestionStore } from '../../store/gestion';
+// import { ref } from 'vue';
 
-import EditCustomer from './EditCustomer.vue';
-import AddCustomer from './AddCustomer.vue';
+// import EditCustomer from './EditCustomer.vue';
+// import AddCustomer from './AddCustomer.vue';
 
-const store = useGestionStore()
+// const store = useGestionStore()
 
-const selectedCustomer = ref(null);
-const add = ref(false)
-const editC = ref(false)
+// const selectedCustomer = ref(null);
+// const add = ref(false)
+// const editC = ref(false)
 
-function editCust(){
-  editC.value = true
-}
+// function editCust(){
+//   editC.value = true
+// }
 
-function newCust(){
-  add.value = true
-}
-function openModal(customer) {
-  selectedCustomer.value = customer;
-}
+// function newCust(){
+//   add.value = true
+// }
+// function openModal(customer) {
+//   selectedCustomer.value = customer;
+// }
 
-function closeModal() {
-  selectedCustomer.value = null;
-}
-
-
+// function closeModal() {
+//   selectedCustomer.value = null;
+// }
 
 
 
-const destroy = () => {
-  if (window.confirm("Confirm the deletion of this client")) {
-    const index = store.customers.findIndex(r => r === selectedCustomer.value);
-    if (index !== -1) {
-      store.removeCustomer(index);
-    }
+
+
+// const destroy = () => {
+//   if (window.confirm("Confirm the deletion of this client")) {
+//     const index = store.customers.findIndex(r => r === selectedCustomer.value);
+//     if (index !== -1) {
+//       store.removeCustomer(index);
+//     }
     
-    router.push("/list-customer");
-  }
-};
+//     router.push("/list-customer");
+//   }
+// };
 </script>
 
 <template >
    <NavBar />
     <div class="container mb-4 mt-4">
-        <h2>List of Customers</h2>
+        <h2>List of Students</h2>
     </div>
     <div class="container d-flex justify-content-end mb-2">
         <button class="btn btn-info" @click="newCust()"><i class="fa fa-user-plus" aria-hidden="true" ></i> Add New Customer</button>
@@ -60,19 +60,22 @@ const destroy = () => {
     <tr>
       <th scope="col">id</th>
       <th scope="col">Name</th>
-      <th scope="col">Adress</th>
+      
       <th scope="col">Email</th>
+      <th scope="col">Role</th>
       <th scope="col">Phone</th>
+      <th scope="col">status</th>
       <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for="(customer, index) in store.customers" :key="index">
-      <th scope="row">{{ index + 1 }}</th>
-      <td>{{customer.name}}</td>
-      <td>{{customer.address}}</td>
-      <td>{{customer.email}}</td>
-      <td>{{customer.phone}}</td>
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
       <td><button type="button" class="btn btn-primary" @click="openModal(customer)"><i class="fa fa-eye" aria-hidden="true"></i></button> <button type="button" class="btn btn-warning" @click="editCust()"><i class="fa fa-pencil" aria-hidden="true"></i></button>  <button type="button" class="btn btn-danger" @click="destroy"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
       
     </tr>
@@ -85,15 +88,15 @@ const destroy = () => {
    <div class="modal-overlay" v-if="selectedCustomer">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">View customer</h5>
+          <h5 class="modal-title">View Student</h5>
           <button type="button" class="btn-close"  aria-label="Close" @click="closeModal"><i class="fa fa-times" aria-hidden="true"></i></button>
         </div>
         <div class="modal-body">
           <ul>
-            <li><strong>Customer Name :</strong> {{ selectedCustomer.name }}</li>
-            <li><strong>Address :</strong> {{ selectedCustomer.address }}</li>
-            <li><strong>Email :</strong> {{ selectedCustomer.email }}</li>
-            <li><strong>Phone :</strong> {{ selectedCustomer.phone }}</li>
+            <li><strong>Student Name :</strong> </li>
+            <li><strong>Address :</strong> </li>
+            <li><strong>Email :</strong> </li>
+            <li><strong>Phone :</strong> </li>
         
           </ul>
         </div>
